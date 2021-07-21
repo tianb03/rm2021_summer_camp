12b09f3b900e518dea8b5dbc4b6956c68bd97639
rocket <2352089873@qq.com>
2021-07-21 08:48:43 +0800

    Day 1 Task

---
+++ testspace/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/hsk/ros/testspace/src
+# Build directory: /home/hsk/ros/testspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_topic")
---
+++ testspace/src/learning_topic/src/vp.cpp
@@ -0,0 +1,22 @@
+#include<ros/ros.h>
+#include<geometry_msgs/Twist.h>
+int main(int argc,char **argv)
+{
+    ros::init(argc,argv,"vp");
+    ros::NodeHandle n;
+    ros::Publisher turtle;
+    turtle = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",10);
+    ros::Rate loop_rate(10);
+    while (ros::ok())
+    {
+        /* code for loop body */
+        geometry_msgs::Twist vel_msg;
+        vel_msg.linear.x=0.5;
+        vel_msg.angular.z=0.2;
+
+        turtle.publish(vel_msg);
+        ROS_INFO("publish turtle x:%0.2f m/s,z:%0.2f rads",vel_msg.linear.x,vel_msg.angular.z);
+        loop_rate.sleep();
+    }
+    
+}

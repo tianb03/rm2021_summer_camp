b8bccc55bc9da2977baf9ad28f5ae129c511c476
ChanisYP <ChanisYP@outlook.com>
2021-07-21 11:20:33 +0800

    Created workspace and initialized

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chanis/rm2021_summer_camp/src
+# Build directory: /home/chanis/rm2021_summer_camp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("chanis")
---
+++ build/chanis/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chanis/rm2021_summer_camp/src/chanis
+# Build directory: /home/chanis/rm2021_summer_camp/build/chanis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
